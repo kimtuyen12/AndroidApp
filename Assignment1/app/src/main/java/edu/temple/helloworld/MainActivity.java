a730e5a9ef0d494d06eb2e915713db6605b6877a
Tuyen Pham <tuh39709@temple.edu>
2020-02-01 22:03:08 -0500

    click behavior for button implemented

--- Assignment1/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ Assignment1/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,14 +1,31 @@
 package edu.temple.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.myText);
+        btn = (Button) findViewById(R.id.myButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("You have clicked the button");
+            }
+        });
+
+
+
     }
+
 }
